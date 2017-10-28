3d38d879322b42dd3ecb0ebf3093573437c1e016
Siddhesh <siddheshmuley1462@gmail.com>
2017-10-28 17:29:36 -0400

    added speech recognition functionality

--- app/src/main/java/com/example/android/moviebud/MainActivity.java
+++ app/src/main/java/com/example/android/moviebud/MainActivity.java
@@ -1,35 +1,55 @@
 package com.example.android.moviebud;
 
-import android.media.AudioRecord;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.CompoundButton;
-import android.widget.ToggleButton;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    AudioRecord recorder;
-    ToggleButton recBtn;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+    Button recBtn;
+    SpeechRecognizer recognizer;
+    Intent recognitionIntent;
+    private final int SPEECH_RECOGNITION_CODE=1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recorder=new AudioRecord(6,//MediaRecorder.AudioSource.VOICE_RECOGNITION
-                recorder.getSampleRate(),
-                16,//CHANNEL_IN_MONO
-                2,//ENCODING_PCM_16BIT
-                recorder.getMinBufferSize(recorder.getSampleRate(),16,2));
-        recBtn=(ToggleButton)findViewById(R.id.recBtn);
-        recBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        recBtn = (Button) findViewById(R.id.recBtn);
+        recBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if(isChecked){
-                    recorder.startRecording();
-                }
-                else{
-                    recorder.stop();
-                    recorder.release();
+            public void onClick(View view) {
+                recognitionIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                //recognizer.startListening(recognitionIntent);
+                recognitionIntent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                        "Say something...");
+                try {
+                    startActivityForResult(recognitionIntent,SPEECH_RECOGNITION_CODE);
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getApplicationContext(),
+                            "Sorry! Speech recognition is not supported in this device.",
+                            Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==SPEECH_RECOGNITION_CODE){
+            TextView tv=(TextView)findViewById(R.id.speech);
+            ArrayList<String>al=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            tv.setText(al.get(0));
+
+        }
     }
 }
